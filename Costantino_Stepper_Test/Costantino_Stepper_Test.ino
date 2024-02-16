e7c69b0a575ab287c1e2ab99f3ac85079a4518aa
Jacob Eddy <jeddy@highpoint.edu>
2024-02-16 11:55:25 -0500

    Update Stepper Test

--- Costantino_Stepper_Test/Costantino_Stepper_Test.ino
+++ Costantino_Stepper_Test/Costantino_Stepper_Test.ino
@@ -26,41 +26,6 @@ void setup() {
 }
 
 void loop() {
-
-}
-
-void moveArmToSquare(double x,  double y) {
-   // Inverse Kinematics Calculation
-   calculateAngles(x, y);
-
-   // Move the SCARA arm to the calculated joint angles
-   moveShoulder();
-   moveElbow();
-   // Optionally, moveWrist and controlGripper functions can be added if needed
-   
-   // Delay to allow the arm to reach the desired position
-   delay(1000);
-}
-
-void calculateAngles(double x, double y) {		// Find Theta1 and Theta2
-   // Inverse Kinematics Calculation
-   double L1 = 7.5;
-   double L2 = 7.5;
-
-   double D = (pow(x,2) + pow(y,2) - pow(L1,2) - pow(L2,2))/ (2 * L1 * L2);
-   theta2 = atan2(-sqrt(1 - pow(D,2)), D);
-    
-   double phi = atan2(y, x); 			// Ensure correct quadrant
-   double c = L1 + L2 * cos(theta2);
-   double s = L2 * sin(theta2);
-   theta1 = phi - atan2(s, c);
-    
-   // Update global variables or pass theta1 and theta2 to the calling function
-}
-
-void moveShoulder() {
-   //Determine if the angular movement is an increase or decrease in angular position
-
   //Call a different function depending on angular movement
   if (currentAngle1 < theta1) {
     moveAngle1 = theta1-currentAngle1; //Create a move angle variable to send to the function
@@ -76,27 +41,6 @@ void moveShoulder() {
   else {
     currentAngle2 = theta2;
   }
-
-}
-
-void moveElbow() {
-   
-  //Call a different function depending on angular movement
-  if (currentAngle1 < theta1) {
-    moveAngle1 = theta1-currentAngle1; //Create a move angle variable to send to the function
-    moveCW(moveAngle1);
-    currentAngle1 = theta1; //Set the new current angle
-    //Serial.println(currentAngle);
-  }
-  else if (currentAngle2 > theta2) {
-    moveAngle2 = currentAngle2-theta2;
-    moveCCW(moveAngle2);
-    currentAngle2 = theta2;
-  }
-  else {
-    currentAngle2 = theta2;
-  }
-
 }
 
 /*This function determines the clockwise angular movement required for the motor
